145cd36d61b1251e20aec77615fbc098cf92fbad
James <jamesmcdowell1897@gmail.com>
2020-01-22 10:10:14 -0500

    changed something small

--- src/OddsAndEvens.java
+++ src/OddsAndEvens.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class OddsAndEvens {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner scnr = new Scanner(System.in);
 		
 		int userNum;
